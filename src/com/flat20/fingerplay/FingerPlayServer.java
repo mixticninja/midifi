b72ed826489c46683f7974271fba92c9809774a2
rto <mixticstudiopro.gmail>
2020-01-05 17:02:41 +0100

    version change 1.0.3

--- src/com/flat20/fingerplay/FingerPlayServer.java
+++ src/com/flat20/fingerplay/FingerPlayServer.java
@@ -26,7 +26,7 @@ import com.flat20.fingerplay.socket.UdpOutput;
 
 public class FingerPlayServer implements Runnable {
 
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	public static final int SERVERPORT = 4444;
 
 	public static final int UDP_SERVERPORT = 9013;
