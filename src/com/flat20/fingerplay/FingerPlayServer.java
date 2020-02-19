f70dc9b545f982960d8fd0fd4db7eb93fe5c9494
rto <mixticstudiopro.gmail>
2020-02-19 13:34:03 +0100

    Server setup UI with TCP listening Port and pre-launch exe

--- src/com/flat20/fingerplay/FingerPlayServer.java
+++ src/com/flat20/fingerplay/FingerPlayServer.java
@@ -29,7 +29,7 @@ import com.flat20.fingerplay.ui.SettingsFrame;
 public class FingerPlayServer implements Runnable {
 
 	public static final String VERSION = "1.0.4";
-	public static final int SERVERPORT = 8080;
+	public static final int SERVERPORT = 4444;
 
 	public static final int UDP_SERVERPORT = 9013;
 
